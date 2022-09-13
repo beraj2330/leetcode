111cf5ca4c69a58625c0b177fda39a7cec8253ad
Jeel Bera <jeelbera2330@gmail.com>
2022-09-13 18:13:20 -0400

    1929. Concantenation of Array

---
+++ Concatenation.py
@@ -0,0 +1,10 @@
+class Solution(object):
+
+    def getConcatenation(self, nums):
+        result = 2*nums
+        
+        return result
+
+a = Solution()
+l = [1,2,1]
+print(a.getConcatenation(l))
