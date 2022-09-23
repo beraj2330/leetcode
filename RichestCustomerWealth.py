b7832be2700fb2c2f879791f62a074651d2a75ea
Jeel Bera <jeelbera2330@gmail.com>
2022-09-23 19:24:06 -0400

    Leetcode Solution

---
+++ RichestCustomerWealth.py
@@ -0,0 +1,14 @@
+class solution():
+    def wealth(self, result: List[List[int]]):
+        max = 0
+        
+
+        for i in range(len(list)):
+            print(list[i])
+        
+
+        return result
+
+a = solution()
+list = [[1,2,3],[3,2,1]]
+print(a.wealth(list[list[int]]))
